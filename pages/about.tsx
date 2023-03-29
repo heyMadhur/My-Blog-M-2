330c41925e51183c99916059de07cd893292bd0f
heyMadhur <madhurgupta12112002@gmail.com>
2023-03-29 21:22:16 +0530

    about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,9 +2,34 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      This is About
-    </div>
+    <>
+      <h1>About Blogger's Blog</h1> 
+      <div>
+      <h2>Introduction</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde aliquam exercitationem sint cupiditate labore tempora temporibus perferendis soluta, porro vero saepe neque, dolor fugit? Reiciendis consectetur ex officia, sit eveniet quia, hic fuga ea omnis eos voluptatem alias adipisci non laborum soluta saepe consequuntur. Vero nihil quibusdam voluptates ullam assumenda dicta sit laboriosam harum facilis quia doloribus illum pariatur recusandae sapiente sint, sed expedita! Totam labore debitis fugit nostrum dolor molestias quidem quo reiciendis illo quasi maxime placeat accusantium error quae dolorum exercitationem veniam autem nihil, saepe in enim officiis iste voluptatibus! Commodi placeat laudantium nesciunt laboriosam, culpa quos deserunt!</p>
+      </div>
+      <div>
+        <h2>Services Offered</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque ea quia ducimus possimus commodi culpa consequatur officiis. Fugiat placeat labore dolore, quo nam, excepturi beatae impedit error perferendis suscipit accusantium, molestias cumque in. Perspiciatis iure cum, placeat quisquam sed culpa beatae! Quo repellendus qui placeat esse, in porro vel natus.</p>
+        <p>We Offer the following the services-:</p>
+        <ul>
+          <li>Service 1</li>
+          <li>Service 2</li>
+          <li>Service 3</li>
+          <li>Service 4</li>
+          <li>Service 5</li>
+          <li>Service 6</li>
+          <li>Service 7</li>
+          <li>Service 8</li>
+        </ul>
+      </div>
+      <div>
+        <h2>Contact Us</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero doloribus temporibus ab maxime praesentium, in explicabo, doloremque autem recusandae voluptatibus quas, illo ratione rerum debitis.</p>
+      </div>
+        
+
+    </>
   )
 }
 
