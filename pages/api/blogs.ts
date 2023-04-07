f30bdfa9aa0249984cb8f44d10463abe55076ff1
heyMadhur <madhurgupta12112002@gmail.com>
2023-04-07 14:06:39 +0530

    test

--- pages/api/blogs.ts
+++ pages/api/blogs.ts
@@ -8,11 +8,11 @@ export default async function handler(
   const allFilesData: string[] = [];
   // res.status(200).json("Request Recieved");
 
-  let data = await fs.promises.readdir(`/blogdata`, "utf-8");
-  data = data.slice(0, parseInt("" + req.query.count));
-  data.forEach((file) => {
-    const fileData = fs.readFileSync(`/blogdata/${file}`, "utf-8");
-    allFilesData.push(JSON.parse(fileData));
-  });
-  res.status(200).json(allFilesData);
+  // let data = await fs.promises.readdir(`/blogdata`, "utf-8");
+  // data = data.slice(0, parseInt("" + req.query.count));
+  // data.forEach((file) => {
+    // const fileData = fs.readFileSync(`/blogdata/${file}`, "utf-8");
+    // allFilesData.push(JSON.parse(fileData));
+  // });
+  res.status(200).json('hi');
 }
