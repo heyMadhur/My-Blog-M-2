edde482a05ed084eb91fb92a9b360bee3e654670
heyMadhur <madhurgupta12112002@gmail.com>
2023-04-07 14:08:50 +0530

    test

--- pages/api/getblog.ts
+++ pages/api/getblog.ts
@@ -10,11 +10,12 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  fs.readFile(`blogdata/${req.query.slug}`, "utf-8", (err, data) => {
-    if (err) {
-      const response: Data = { error: "Blog not Found" };
-      res.status(500).json(response);
-    }
-    res.status(200).json(JSON.parse(data));
-  });
+  // fs.readFile(`blogdata/${req.query.slug}`, "utf-8", (err, data) => {
+  //   if (err) {
+  //     const response: Data = { error: "Blog not Found" };
+  //     res.status(500).json(response);
+  //   }
+    // res.status(200).json(JSON.parse(data));
+  // });
+  res.status(200);
 }
